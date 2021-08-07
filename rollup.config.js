073b71aa85f317ddf789383e15969d0c28620050
cdellacqua <carlo.dellacqua97@gmail.com>
2021-08-07 13:36:13 +0200

    change packaging: add exports and use rollup to compile esm and cjs format

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+const external = [
+	...Object.keys(pkg.devDependencies || {}),
+	...Object.keys(pkg.peerDependencies || {}),
+	...Object.keys(pkg.dependencies || {}),
+];
+
+export default [
+	{
+		input: 'src/index.ts',
+		external,
+		plugins: [
+			typescript()
+		],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
